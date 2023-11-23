57f465cc3100210a555a48ef137b331faf62efcb
Logan Matheny <loganpaulmatheny@gmail.com>
2023-11-23 12:47:29 -0500

    Style the articles layout

--- src/Components/Articles/Articles.js
+++ src/Components/Articles/Articles.js
@@ -5,7 +5,14 @@ import Card from "../Card/Card";
 function Articles({ articles }) {
   console.log(articles);
   const articleCards = articles.map((article) => {
-    return <Card id={article.id} key={article.id} title={article.title} />;
+    return (
+      <Card
+        id={article.id}
+        key={article.id}
+        title={article.title}
+        description={article.description}
+      />
+    );
   });
 
   return <div className="articles-container">{articleCards}</div>;
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,11 +2,12 @@ import "./Card.css";
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function Card({ title, id }) {
+function Card({ title, id, description }) {
   return (
     <NavLink to={`/article/${id}`} className="nav-link">
       <div className="card">
         <h3>{title}</h3>
+        <p>{description}</p>
       </div>
     </NavLink>
   );
